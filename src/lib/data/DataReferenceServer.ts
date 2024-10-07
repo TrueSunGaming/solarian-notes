3744edd4f4aae0352a92a71e153956e6041b834d
TrueSunGaming <truesungaming@gmail.com>
2024-10-07 14:16:44 +0000

    feat: add Task to DataReferenceServer

--- src/lib/data/DataReferenceServer.ts
+++ src/lib/data/DataReferenceServer.ts
@@ -3,12 +3,16 @@ import type { DataTypeInstance } from "./DataTypeInstance";
 import { getUserByID } from "$lib/db/access/user/getUserByID";
 import type { WithId } from "mongodb";
 import { DataReference } from "./DataReference";
+import { getTaskByID } from "$lib/db/access/task/getTaskByID";
 
 export class DataReferenceServer<T extends DataType> extends DataReference<T> {
     override get value(): Promise<WithId<DataTypeInstance<T>> | null> {
         switch (this.type) {
             case DataType.User:
                 return getUserByID(this.id) as Promise<WithId<DataTypeInstance<T>> | null>;
+            
+            case DataType.Task:
+                return getTaskByID(this.id) as Promise<WithId<DataTypeInstance<T>> | null>;
             
             default:  
                 throw new Error("Invalid DataType in DataReferenceServer");
