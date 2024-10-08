60cc10d42c7752941b32e99f642df186e0c2478d
TrueSunGaming <truesungaming@gmail.com>
2024-10-08 16:45:41 +0000

    feat: implement DataReferenceClient

---
+++ src/lib/data/DataReferenceClient.ts
@@ -0,0 +1,11 @@
+import type { DataType } from "./DataType";
+import { DataReference } from "./DataReference";
+import type { WithId } from "mongodb";
+import type { DataTypeInstance } from "./DataTypeInstance";
+
+export class DataReferenceClient<T extends DataType> extends DataReference<T> {
+    override get value(): Promise<WithId<DataTypeInstance<T>> | null> {
+        return fetch(`/api/get-document/${this.type}/${this.id}`)
+        .catch(() => null).then((r) => r?.json().catch(() => null));
+    }
+}
