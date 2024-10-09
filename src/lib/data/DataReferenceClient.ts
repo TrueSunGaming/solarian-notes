a0ee2fda384976d138710449ea013c18f5037250
TrueSunGaming <truesungaming@gmail.com>
2024-10-09 14:21:55 +0000

    style: remove newline

--- src/lib/data/DataReferenceClient.ts
+++ src/lib/data/DataReferenceClient.ts
@@ -5,7 +5,6 @@ import type { DataTypeInstance } from "./DataTypeInstance";
 
 export class DataReferenceClient<T extends DataType> extends DataReference<T> {
     override get value(): Promise<WithId<DataTypeInstance<T>> | null> {
-        return fetch(`/api/get-document/${this.type}/${this.id}`)
-        .catch(() => null).then((r) => r?.json().catch(() => null));
+        return fetch(`/api/get-document/${this.type}/${this.id}`).catch(() => null).then((r) => r?.json().catch(() => null));
     }
 }
